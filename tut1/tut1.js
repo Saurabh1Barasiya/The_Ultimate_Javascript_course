00e3079c0c83eb79887d4d1cc55ebc27073b61ca
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-16 18:04:15 +0530

    How javascript is dynamiclly type.

---
+++ tut1/tut1.js
@@ -0,0 +1,17 @@
+// cteate a variable .
+
+// we use let we create variable.
+
+// javaScript is a dynamic language.
+
+let a = 10;
+console.log(a,typeof a);
+
+a = "Saurabh";
+console.log(a,typeof a);
+
+a = 10.55;
+console.log(a,typeof a);
+
+a = true;
+console.log(a,typeof a);
