99fc4df3c95e3a64e4f6a865e7299d61859dda12
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-21 20:56:45 +0530

    solve some practice set.

---
+++ tut4/tut4.js
@@ -0,0 +1,37 @@
+console.log("All about practice set.")
+
+
+// add Saurabh to 1 digit ...
+
+let name = "Saurabh";
+name = name+1;
+console.log(name);
+
+// print the type .
+console.log(typeof name);
+
+const a1 = {
+    name: "Saurabh",
+    age: 21,
+    city: "Pune"
+}
+
+// a1 = "some"; //  Assignment to constant variable.
+console.log(a1);
+
+// add a new property to the object.
+a1["adderss"] = "Harrai";
+console.log(a1);
+
+
+
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+
+console.log(dict.appreciate);
+console.log(dict.yakka);
+console.log(dict["ataraxia"]);
+
