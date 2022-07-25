da3214701eb37c3dd46c9fa27842bdd72d61ab0e
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-25 10:47:56 +0530

    To know about switch case.

---
+++ tut7/tut7.js
@@ -0,0 +1,26 @@
+let number = Number.parseInt(prompt("Enter a number"));
+switch (number) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    default:
+        console.log("number is not between 0 and 6");
+}
