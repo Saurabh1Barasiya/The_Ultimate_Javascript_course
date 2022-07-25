7ba8fe89c512d56d035964e45c7aa9fa18c09420
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-25 10:28:22 +0530

    To know about Operators.

---
+++ tut5/tut5.js
@@ -0,0 +1,50 @@
+console.log("To know about operators in javascript");
+
+let a = 5;
+let b = 5;
+let c = 2;
+console.log("a+b", a+b);
+console.log("a-b", a-b);
+console.log("a*b", a*b);
+console.log("a/b", a/b);
+
+console.log("a % c ",a%c);
+
+console.log("a**c", a**c);
+
+// pahle print kero fir increment kero.
+console.log("c++", c++);
+console.log("c", c);
+
+// pahle print kero fir decrement kero.
+console.log("c--", c--);
+console.log("c", c);
+
+
+// pahle increment kar do fir print kero.
+
+console.log("++c", ++c);
+
+// pahle decrement kar do fir print kero.
+
+console.log("--c", --c);
+
+
+
+// now we know about == AND === operators .
+
+let value1 = 10 ;
+let value2 = "10";
+
+console.log("value1 == value2", value1 == value2);
+console.log("value1 === value2", value1 === value2);
+
+// to == operator only data (content) par worked karta  h.
+
+// and === operator data type ko check karta h . matlab dono ka type bhi sahi hona chahiye .
+// same work as !== operator.
+
+//                                   10!="10"
+console.log("value1 != value2", value1 != value2);
+console.log("value1 != value2", value1 !== value2);
+
