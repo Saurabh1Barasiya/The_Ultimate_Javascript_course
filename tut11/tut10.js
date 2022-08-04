10ab46e43536f4465d51d49883046eb400af91ff
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-08-04 22:47:18 +0530

    Know about string.

---
+++ tut11/tut10.js
@@ -0,0 +1,31 @@
+console.log("Know about string");
+
+let name = "Saurabh Barasiya";
+console.log(name);
+
+let friend_name = 'Suhani';
+console.log(friend_name);
+
+let more = `aese bhi create kar sakte h.`;
+console.log(more);
+
+let myStr = "this is string";
+console.log(typeof myStr);
+
+
+console.log(`${name} and ${friend_name}`);
+
+
+for(let i of name){
+    console.log(i)
+}
+
+
+// escape sequesnce.
+
+let val = "it\"s";
+console.log(val);
+
+let val1 = 'it\'s';
+console.log(val1);
+
