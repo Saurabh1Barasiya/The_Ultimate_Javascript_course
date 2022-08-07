d83b1cd9043164ca356146b38953b9fe519510aa
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-08-07 18:51:32 +0530

    string methods

---
+++ tut13/tut13.js
@@ -0,0 +1,39 @@
+// const browserType = 'mozilla';
+
+// if (browserType.includes('zilla')) {
+//   console.log('Found zilla!');
+// } else {
+//   console.log('No zilla here!');
+// }
+
+
+
+
+// const browserType = 'mozilla';
+// if (browserType.startsWith('zilla')) {
+//   console.log('yes');
+// } else {
+//   console.log('No ');
+// }
+
+
+
+
+
+// const browserType = 'mozilla';
+
+// if (browserType.endsWith('zilla')) {
+//   console.log('yes');
+// } else {
+//   console.log('No');
+// }
+
+
+
+
+
+const tagline = 'xMDN - Resources for developers, by developers';
+// console.log(tagline.indexOf('developers')); // 20
+
+console.log(tagline.indexOf('x',4)); // -1
+// 4 KE BAAD SE SEARCH KARNA START KERO.
