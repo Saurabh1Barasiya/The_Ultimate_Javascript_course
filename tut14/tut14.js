346b93bb3c2b19063bd404d2f51c756ff87ae20e
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-08-08 23:55:40 +0530

    Array method push pop shift and unshift.

---
+++ tut14/tut14.js
@@ -0,0 +1,83 @@
+
+/*
+console.log("Here we learn about Array");
+// const arr = [10,20,30,40,50,60]
+// console.log(arr);
+
+
+// for of loop is a good in array.
+// for(let ele of arr) {
+//     console.log(ele);
+// }
+
+// insert in last position to a array.
+// push method return a new length of array.
+let a = arr.push(70);
+console.log(arr);
+console.log("New lenght of array is ",a);
+
+// delete last element of array.
+let b = arr.pop();
+console.log(arr);
+console.log("Last element of array is ",b);
+
+// push last me insert karta h . and new lenght return karta h.
+// pop last element delete karta h.  and deleted value return karta h.
+
+
+
+
+// ================================.
+
+console.log(arr);
+// insert in first position to a array.
+let c = arr.unshift(0);
+console.log(arr);
+console.log("New lenght of array is ",c);
+
+// unshift first element insert karta h. and new lenght return karta h.
+// shift first element delete karta h.  and deleted value return karta h.
+
+let ab = arr.shift();
+console.log(arr,ab);
+
+*/
+
+
+
+
+// will dicuss about array.
+
+// const arr = [1,2,3,4,5,6];
+// const arr = ["a","b","c","d","e","f"];
+
+// console.log(arr);
+
+// for(let ele of arr) {
+//     console.log(ele);
+// }
+
+
+// 👀👀👀👀👀
+// remember one thing forEach ka first argument element , second argument index , third argument array hoga.
+
+//              element,index,array
+// arr.forEach((a,b,c)=> {
+//     console.log(a,b,c);
+// })
+
+
+
+// 👀👀👀👀👀👀👀👀👀.
+// know about array methods.
+
+// push() == > if you want to insert in last position.
+// push method  return karta h. new length of array .
+// const arr = ["a","b","c"];  // 
+// let newLength = arr.push("d");
+// console.log(arr,newLength);
+
+// let popperElemment =  arr.pop();
+// console.log(arr,popperElemment);
+
+
