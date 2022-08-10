2790281fa900653ec3dace36fa2cb1dac82a9cae
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-08-10 15:04:36 +0530

    About and object destructring.

---
+++ tut20/tut20.js
@@ -0,0 +1,72 @@
+console.log("About objects");
+
+const person = {
+    name: "John",
+    age: 30,
+    hobbies: ["movies", "music"],
+}
+
+// console.log(person);
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+
+// for(let hob of person.hobbies){
+//     console.log(hob);
+// }
+
+
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+
+// const cars = [
+//     {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+//     {name:"BMW", models:["320", "X3", "X5"]},
+//     {name:"Fiat", models:["500", "Panda"]}
+// ];
+
+// const [first,secound,third] = cars;
+
+// console.log(first);
+// console.log(secound);
+// console.log(third);
+
+// for(let car of cars){
+//     // console.log(car);
+//     for(let item in car){
+//         console.log(`${item}  : ${car[item]}`);
+//     }
+// }
+
+
+
+// object destructring.
+// const {name,age,hobbies:like} = person;
+// yaha par like new name h hobbies ka.
+
+
+
+// console.log(name,age,like);
+
+
+// Object destructuring
+const users = [
+    {userId:1, name:"John", age:20,gender:"male"},
+    {userId:2, name:"rohn", age:21,gender:"male"},
+    {userId:3, name:"thon", age:30,gender:"male"},
+];
+
+// const [user1,user2,user3] = users;
+// console.log(user1, user2, user3);
+
+
+const [{userId:firstID,name:firstName,age:firstAge},{userId:secoundID,name:secoundName,age:secoundAge},{userId:thirdId,name:thirdName,age:thirdAge},] = users;
+
+console.log(firstID,firstName,firstAge);
+console.log(secoundID,secoundName,secoundAge);
+console.log(thirdId,thirdName,thirdAge);
+
+
