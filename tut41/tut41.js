1a983e46b3345f75d127a548d446c7872e882025
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-08-18 13:34:33 +0530

    Optional chaining.

---
+++ tut41/tut41.js
@@ -0,0 +1,13 @@
+// Optional chaining.
+
+console.log("Optional chaining");
+
+const user = {
+    name: "John",
+    address: {housenumber:'1234'}
+}
+
+
+console.log(user?.name);
+console.log(user?.address?.housenumber);
+console.log("Housenumber" ,user["address"]["housenumber"]);
